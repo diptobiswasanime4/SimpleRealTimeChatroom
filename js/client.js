093e88520bdbb7be0e6f83723c2cae29a7934df0
diptobiswasanime4 <diptobiswasanime4@gmail.com>
2022-06-26 17:27:57 +0530

    [Dipto] Refactor: Changing URL

--- js/client.js
+++ js/client.js
@@ -1,4 +1,4 @@
-const socket = io("http://localhost:8000");
+const socket = io("http://127.0.0.1:8000:8000");
 
 const form = document.getElementById("send-container");
 const messageInput = document.getElementById("messageInp");
